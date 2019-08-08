66e145025561ca0301a0e6023119e7a3da329db2
Pavel Kolesnikov <pavel.kolesnikov@gmail.com>
2019-08-07 20:13:48 -0700

    RunQuery can accept a query from the command line

--- src/com/gooddata/maql/jdbc/driver/RunQuery.java
+++ src/com/gooddata/maql/jdbc/driver/RunQuery.java
@@ -3,24 +3,35 @@ package com.gooddata.maql.jdbc.driver;
 import java.sql.DriverManager;
 
 public class RunQuery {
-
+  public static final String DEFAULT_QUERY = "SELECT Product, Revenue";
+ 
   public static void main(String[] args) throws Exception {
-    if (args.length != 3) {
+    if (args.length < 3) {
       throw new IllegalArgumentException("Usage: RunQuery <jdbcUrl> <username> <password>");
     }
     String jdbcUrl = args[0];
     String username = args[1];
     String password = args[2];
+    String query = args.length > 3 ? args[3] : DEFAULT_QUERY;
+    System.out.println(query);
     
-    DriverManager.registerDriver (new Driver());
+    // DriverManager.registerDriver (new Driver());
     java.sql.Connection conn = DriverManager.getConnection(jdbcUrl, username, password);
     java.sql.Statement stmt = conn.createStatement();
-    java.sql.ResultSet rs = stmt.executeQuery("SELECT Product, Revenue");
+    java.sql.ResultSet rs = stmt.executeQuery(query);
     if (rs == null) {
       throw new IllegalStateException("null ResultSet");
     }
+    int columnCount = rs.getMetaData().getColumnCount();
     while (rs.next()) {
-      System.out.printf("%s\t%s\n", rs.getString(1), rs.getString(2));
+      StringBuffer sb = new StringBuffer();
+      for (int i = 1; i <= columnCount; i++) {
+        sb.append(rs.getString(i));
+        if (i != columnCount) {
+          sb.append("\t");
+        }
+      }
+      System.out.println(sb.toString());
     }
     rs.close();
     stmt.close();
